01aebaf6e25f3bd3b413dc63e3c68b3bc46c399b
Girish-Adapa <girishsaiadapa1999@gmail.com>
2023-08-19 14:36:53 +0530

    bbellam

--- src/FormPage.jsx
+++ src/FormPage.jsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { Form, Input, Button, message } from 'antd';
 import { WhatsAppOutlined } from '@ant-design/icons';
+import WhatsApp from './WhatsApp';
+import Instagram from './Instagram';
 import './FormPage.css';
 
 // import pic1 from './images/IMG_20230127_211815.jpg'
@@ -38,67 +40,74 @@ const FormPage = () => {
   };
 
   return (
-    <div className="center-container">
-      <div className="form-container">
-        <h2 className="form-title">SSC 2K15 REUNION</h2>
-        <Form
-          name="data-collection-form"
-          onFinish={onFinish}
-          labelCol={{ span: 6 }}
-          wrapperCol={{ span: 16 }}
-        >
-          <Form.Item
-            label="Name"
-            name="name"
+    <>
+      <div className="center-container">
+        <div className="form-container">
+          <h2 className="form-title">SSC 2K15 REUNION</h2>
+          <Form
+            name="data-collection-form"
+            onFinish={onFinish}
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 16 }}
           >
-            <Input
-              type="text"
-              placeholder='Enter your Full Name'
-              onChange={(e) => setName(e.target.value)}
-              className="input-field"
-            />
-          </Form.Item>
+            <Form.Item
+              label="Full Name"
+              name="name"
+            >
+              <Input
+                type="text"
+                placeholder='Enter your Full Name'
+                onChange={(e) => setName(e.target.value)}
+                className="input-field"
+              />
+            </Form.Item>
 
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              { type: 'email', message: 'Please enter a valid email' },
-            ]}
-          >
-            <Input
-              placeholder='Enter your Email'
-              onChange={(e) => setEmail(e.target.value)}
-              className="input-field"
-            />
-          </Form.Item>
+            <Form.Item
+              label="Email Address"
+              name="email"
+              rules={[
+                { type: 'email', message: 'Please enter a valid email' },
+              ]}
+            >
+              <Input
+                placeholder='Enter your Email'
+                onChange={(e) => setEmail(e.target.value)}
+                className="input-field"
+              />
+            </Form.Item>
 
-          <Form.Item
-            label="Number"
-            name="number"
-            rules={[
-              {
-                pattern: /^[0-9]*$/,
-                message: 'Please enter a valid phone number',
-              },
-            ]}
-          >
-            <Input
-              placeholder='Enter your WhatsApp Number'
-              onChange={(e) => setNumber(e.target.value)}
-              className="input-field"
-            />
-          </Form.Item>
+            <Form.Item
+              label="Contact Number"
+              name="number"
+              rules={[
+                {
+                  pattern: /^[0-9]*$/,
+                  message: 'Please enter a valid number',
+                },
+              ]}
+            >
+              <Input
+                placeholder='Enter your WhatsApp Number'
+                onChange={(e) => setNumber(e.target.value)}
+                className="input-field"
+              />
+            </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
-            <Button className="submit-button" type='primary' htmlType='submit'>
-              <WhatsAppOutlined /> Submit
-            </Button>
-          </Form.Item>
-        </Form>
+            <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+              <Button className="submit-button" type='primary' htmlType='submit'>
+                <WhatsAppOutlined /> Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
-      
-    </div>
+      <div className="footer-container">
+        <div className='row'>
+          <WhatsApp className="footer-icon" />
+          <Instagram className="footer-icon" />
+        </div>
+      </div>
+    </>
   );
 };
 
